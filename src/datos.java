1ba9c09d591e906a1d19229b3031865a1f50ce42
Marco9419 <84870105+Marco9419@users.noreply.github.com>
2021-07-20 10:16:16 -0500

    Add files via upload

---
+++ src/datos.java
@@ -0,0 +1,28 @@
+
+public class datos {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+			System.out.println("Hello world!");
+			
+			int numberOne = 10;
+			int NomberTwo = 5;
+			int multiply = numberOne * NomberTwo;
+			String stringOne ="text 1";
+			String stringTwo = "text 2";
+			char singleLetter = 'A';
+			
+			String concat = stringOne + stringTwo;
+			
+			boolean olderThan18 = true;
+			
+			System.out.println(multiply);
+			System.out.println(singleLetter);
+			System.out.println(olderThan18);
+			System.out.println(concat);
+		}
+	}
+
+	
+
